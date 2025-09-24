60b0ed9b6ab60c9ffb66403fb35447934c421e10
Hamza Hamid <hamza.hamid9055@gmail.com>
2025-09-24 22:24:41 +0500

    comit

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
+  eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
-  basePath: isProd ? "/Portfolio" : "",
-  assetPrefix: isProd ? "/Portfolio/" : "",
 };
 
 module.exports = nextConfig;
